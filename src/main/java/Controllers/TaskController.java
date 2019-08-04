2b963ca92727c7f3936038c89e22a17e919dac3d
tomaszlach <tomaszlach86@gmail.com>
2019-08-04 22:23:25 +0200

    added all functionalities

--- src/main/java/Controllers/TaskController.java
+++ src/main/java/Controllers/TaskController.java
@@ -7,7 +7,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
@@ -51,11 +50,12 @@ public class TaskController {
                     handleDisplayAllSorted();
                     break;
                 case "4":
-                    System.out.println(ConsoleColors.CYAN+"podaj ID zadania"+ConsoleColors.RED);
-                    markAsDone(scanner.nextLong());
+                    System.out.println(ConsoleColors.CYAN + "podaj ID zadania" + ConsoleColors.Reset);
+                    updateTask(scanner.nextLong(), displayMarkAsDoneText());
                     break;
                 case "5":
-
+                    System.out.println(ConsoleColors.CYAN + "podaj ID zadania" + ConsoleColors.RESET);
+                    updateTask(scanner.nextLong(), displayUpdateText());
                     break;
                 case "6":
                     handleDisplayAll("", true);
@@ -70,23 +70,68 @@ public class TaskController {
     }
 
 
+    public Task displayUpdateText() {
+        Task temporaryTask = new Task();
 
-    private void markAsDone(Long id){
+        System.out.println("podaj nowy opis. Wciśnij Enter aby zostawić jak jest");
+        String newDescription = scanner.next();
+        newDescription = newDescription.equals("") ? temporaryTask.getDescription() : scanner.nextLine();
+        temporaryTask.setDescription(newDescription);
+
+        System.out.println("podaj kategorię.  Wciśnij Enter aby zostawić jak jest");
+        String newCategory = scanner.next();
+        Category category = newCategory.equals("") ? temporaryTask.getCategory() : Category.valueOf(scanner.nextLine());
+        temporaryTask.setCategory(category);
+
+        System.out.println("Czy zadanie jest gotowe? true/false?");
+        boolean isReady = scanner.nextBoolean();
+        temporaryTask.setReady(isReady);
+
+        System.out.println("podaj datę rozpoczęcia wykonywania zadania?. Wciśnij Enter aby zostawić jak jest");
+        LocalDate startDate = LocalDate.parse(scanner.nextLine());
+        temporaryTask.setStartDate(startDate);
+
+        System.out.println("podaj datę zakończenia wykonywania zadania?. Wciśnij Enter aby zostawić jak jest");
+        LocalDate finishDate = LocalDate.parse(scanner.nextLine());
+        temporaryTask.setFinishDate(finishDate);
+
+        return temporaryTask;
+    }
+
+    public Task displayMarkAsDoneText() {
+        Task temporaryTask = new Task();
+
+        System.out.println("Czy zadanie jest gotowe? true/false?");
+        boolean isReady = scanner.nextBoolean();
+        temporaryTask.setReady(isReady);
+
+        return temporaryTask;
+    }
 
 
+    private void updateTask(Long id, Task temporaryTask) {
         entityManager.getTransaction().begin();
         Task task = entityManager.find(Task.class, id);
-        task.setReady(true);
+
+        task.setDescription(temporaryTask.getDescription());
+        task.setCategory(temporaryTask.getCategory());
+        task.setReady(temporaryTask.isReady());
+        task.setStartDate(temporaryTask.getStartDate());
+        task.setFinishDate(temporaryTask.getFinishDate());
+
         entityManager.persist(task);
         entityManager.getTransaction().commit();
-
     }
+
 
     private void addTestData() {
         entityManager.getTransaction().begin();
-        entityManager.persist(new Task("Kup Masło", Category.HOUSEHOLD, false, LocalDate.of(2019, 8,26)));
-        entityManager.persist(new Task("Idź do okulisty", Category.PRIVATE, false,  LocalDate.of(2019, 6,28)));
-        entityManager.persist(new Task("Wyślij Raport", Category.WORK, false,  LocalDate.of(2019, 10,28)));
+        entityManager.persist(new Task("Kup Masło", Category.HOUSEHOLD, false, LocalDate.of(2019, 8, 26)));
+        Task task = new Task("Idź do okulisty", Category.PRIVATE, true, LocalDate.of(2019, 6, 28));
+        task.setStartDate(LocalDate.of(2019, 6, 5));
+        task.setFinishDate(LocalDate.of(2019, 6, 25));
+        entityManager.persist(task);
+        entityManager.persist(new Task("Wyślij Raport", Category.WORK, false, LocalDate.of(2019, 10, 28)));
         entityManager.getTransaction().commit();
     }
 
@@ -95,7 +140,7 @@ public class TaskController {
         String description = scanner.nextLine();
         System.out.println("Podaj Kategorię");
 
-        for(Category value : Category.values()){
+        for (Category value : Category.values()) {
             System.out.println(value);
         }
         System.out.println(Category.values());
@@ -104,7 +149,7 @@ public class TaskController {
         System.out.println("podaj deadline w formacie RRRR-MM-DD");
         LocalDate ld = LocalDate.parse(scanner.nextLine());
 
-       Task task = new Task();
+        Task task = new Task();
         task.setDescription(description);
         task.setCategory(Category.valueOf(category));
         task.setDeadline(ld);
@@ -121,7 +166,7 @@ public class TaskController {
 
         String orderBy = "";
 
-        if(userInput.equals("N")) {
+        if (userInput.equals("N")) {
             orderBy = "t.description";
         } else if (userInput.equals("C")) {
             orderBy = "t.category";
